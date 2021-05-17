82389d6d70ae20fe7397907f587cd6579864c2ce
Maksym <maksshevchenko5555@gmail.com>
2021-05-17 22:22:02 +0300

    mobile demo improvements

--- src/fullpage.js
+++ src/fullpage.js
@@ -3,9 +3,10 @@ const FullPage = function (selector, options = {}) {
 	this.current_slide = 0;
 	this.options = options;
 	this.slide_count = null;
-	this.current_slide = null;
+	this.current_slide = 0;
 	this.user_height = null;
 	this.animating = false;
+	this.accumulated_touchmove = 0;
 };
 
 FullPage.prototype.init = function () {
@@ -37,16 +38,36 @@ FullPage.prototype.init = function () {
 			touch_start = e.changedTouches["0"].clientY;
 		});
 		window.addEventListener("touchend", (e) => {
-			touch_start > e.changedTouches["0"].clientY
-				? this.moveNext()
-				: this.movePrev();
+			if (this.accumulated_touchmove > this.user_height / 3) {
+				if (this.current_slide != this.slide_count - 1) {
+					this.moveNext();
+				} else {
+					this.reRender();
+				}
+			} else if (-this.accumulated_touchmove > this.user_height / 3) {
+				if (this.current_slide != 0) {
+					this.movePrev();
+				} else {
+					this.reRender();
+				}
+			} else {
+				this.reRender();
+			}
+			this.accumulated_touchmove = 0;
+		});
+
+		window.addEventListener("touchmove", (e) => {
+			this.accumulated_touchmove = Math.floor(
+				touch_start - e.changedTouches["0"].clientY
+			);
+			this.movePrec(this.accumulated_touchmove);
 		});
 	}
 };
 
 FullPage.prototype.reRender = function () {
-	this.$root.style = `transform: translate3d(0px, -${
-		this.current_slide * this.user_height
+	this.$root.style = `transform: translate3d(0px, ${
+		this.current_slide * this.user_height * -1
 	}px, 0px); transition: all 1000ms ease 0s; height: 100%; position: relative;touch-action: none;`;
 };
 
@@ -55,8 +76,8 @@ FullPage.prototype.moveNext = function () {
 		this.animating = true;
 		setTimeout(() => (this.animating = false), 1000);
 		this.current_slide++;
-		this.$root.style = `transform: translate3d(0px, -${
-			this.current_slide * this.user_height
+		this.$root.style = `transform: translate3d(0px, ${
+			this.current_slide * this.user_height * -1
 		}px, 0px); transition: all 1000ms ease 0s; height: 100%; position: relative;touch-action: none;`;
 	}
 };
@@ -66,8 +87,30 @@ FullPage.prototype.movePrev = function () {
 		this.animating = true;
 		setTimeout(() => (this.animating = false), 1000);
 		this.current_slide--;
-		this.$root.style = `transform: translate3d(0px, -${
-			this.current_slide * this.user_height
+		this.$root.style = `transform: translate3d(0px, ${
+			this.current_slide * this.user_height * -1
 		}px, 0px); transition: all 1000ms ease 0s; height: 100%; position: relative;touch-action: none;`;
 	}
+};
+
+FullPage.prototype.moveTo = function (to_slide) {
+	if (to_slide >= 0 && to_slide < this.slide_count && !this.animating) {
+		this.current_slide = to_slide;
+		this.animating = true;
+		setTimeout(() => (this.animating = false), 1000);
+		this.$root.style = `transform: translate3d(0px, ${
+			this.current_slide * this.user_height * -1
+		}px, 0px); transition: all 1000ms ease 0s; height: 100%; position: relative;touch-action: none;`;
+	}
+};
+
+FullPage.prototype.movePrec = function (y) {
+	console.log(
+		this.user_height,
+		this.slide_count,
+		y + this.user_height * this.current_slide
+	);
+	this.$root.style = `transform: translate3d(0px, ${
+		(y + this.user_height * this.current_slide) * -1
+	}px, 0px); height: 100%; position: relative;touch-action: none;`;
 };
